0054ba2f9f27675a25f831c0fbc5a84aeb4146bb
ddt_nakayama <ddt.nakayama@gmail.com>
2022-10-07 16:01:20 +0900

    演習2-10

---
+++ Ensyu2_10.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ensyu2_10 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("姓：");
+		String str1 = scan.nextLine();
+		System.out.print("名：");
+		String str2 = scan.nextLine();
+
+		System.out.println("こんにちは" + str1 + str2 + "さん。");
+	}
+
+}
