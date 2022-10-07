b0197a2b0d457cd02b9138ec75d941f537b784bb
ddt_nakayama <ddt.nakayama@gmail.com>
2022-10-07 21:13:43 +0900

    演習4-20

---
+++ Ensyu4_20.java
@@ -0,0 +1,21 @@
+
+public class Ensyu4_20 {
+
+	public static void main(String[] args) {
+
+		// 辺の長さ
+		final int INT_SIDE = 5;
+
+		// 縦
+		for (int i = 0; INT_SIDE > i; i++) {
+
+			// 横
+			for (int j = 0; INT_SIDE > j; j++) {
+				System.out.print('*');
+			}
+			// 改行
+			System.out.println();
+		}
+	}
+
+}
